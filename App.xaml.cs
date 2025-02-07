32327c7f30c2064a8748ead43c652e32dac4da99
VTischer <viktor.t1@schule.bremen.de>
2025-02-07 10:52:28 +0100

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Book2gather {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+
+}
